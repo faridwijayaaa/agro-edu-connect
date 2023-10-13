b289ea3ce0244a5489d6c5505b4d7dd5f70ba9ee
faridwijayaaa <faridwijayaaa@gmail.com>
2023-10-14 05:31:22 +0700

    confirms function getLiveSession

--- js/ls.js
+++ js/ls.js
@@ -11,17 +11,21 @@ async function getLiveSession() {
 
     liveSession.map((item) => {
       let cardLiveSession = `
-          <div class="card text-center mb-3" style="width: 18rem;">
-              <img src=${item.imgUrl} class="card-img-top" alt="..." />
-              <div class="card-body">
-              <h5 class="card-title">${item.name}</h5>
-              <p class="card-text">
-                  ${item.description.slice(
-                    0,
-                    item.description.indexOf(".") + 1
-                  )}
-              </p>
-              <a href="#" class="btn btn-primary">Detail</a>
+          <div class="card text-center mb-5 border-0 shadow-none pe-none" style="width: 24%; height: 47vh">
+              <img src=${
+                item.imgUrl
+              } class="card-img-top mx-auto" alt="..." style="width: 10vw; height:10vw;" />
+              <div class="card-body container-fluid ps-0">
+                <h5 class="card-title" style="font-size: 0.9rem">${
+                  item.name
+                }</h5>
+                <p class="card-text" style="font-size: 0.8rem">
+                    ${item.description.slice(
+                      0,
+                      item.description.indexOf(".") + 1
+                    )}
+                </p>
+                <a href="#" class="btn btn-primary pe-auto position-absolute bottom-0 start-5 translate-middle-x fs-6 py-1 fw-medium">Detail</a>
               </div>
           </div>
           `;
