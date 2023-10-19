3045aa53f235b9b741ecba81d8f0e141fd8e826a
Aryab13 <139896555+Aryab13@users.noreply.github.com>
2023-10-19 10:01:10 +0700

    Add files via upload

---
+++ loginnnn/forgotpass.js
@@ -0,0 +1,49 @@
+const BASE_API = "https://652ff3566c756603295dfc8e.mockapi.io";
+
+const fetcher = async ({ endpoint = "", config = {} }) => {
+  let result = {
+    data: [],
+    error: {},
+  };
+
+  try {
+    const response = await fetch(`${BASE_API}/${endpoint}`, config);
+    const data = await response.json();
+
+    console.log(data, "data");
+
+    result.data = data;
+  } catch (error) {
+    result.error = error;
+  }
+
+  return result;
+};
+
+const handleSubmit = async (e) => {
+  e.preventDefault();
+
+  const inputEmail = document.getElementById("email").value;
+  const inputPassword = document.getElementById("password").value;
+
+  const { data } = await fetcher({
+    endpoint: "user",
+  });
+
+  const isUserExist = data?.find(
+    (item) => item.email === inputEmail && item.password === inputPassword
+  );
+
+  if (!isUserExist) return alert("Wrong Email");
+
+  localStorage.setItem(
+    "usr",
+    JSON.stringify({
+      id: isUserExist.id,
+      name: isUserExist.name,
+    })
+  );
+  return alert("Reset Password Successful");
+};
+
+document.getElementById("form").onsubmit = handleSubmit;
